1a839c96dea656a7b34abbd9bbc25f47e45e5caa
tushar453030 <93340408+tushar453030@users.noreply.github.com>
2023-07-18 23:39:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 }).catch((err)=>{
     console.log(err);
 });
-
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:5000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200 
+}
+app.use(cors(corsOptions));
 app.use(express.json()); // By this we can able to pass JSON via body as req 
 app.use("/api/auth",authRoute);
 app.use("/api/users", userRoute);
