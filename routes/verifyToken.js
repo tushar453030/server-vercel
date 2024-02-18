385eed89bb3f68c9c9773eaa4cb440a7fbbf04e2
Tushar Sharma <tushar4530@gmail.com>
2024-02-18 22:40:49 +0530

    vercel config

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,43 +1,44 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
-const verifyToken = (req,res,next) => {
-    const authHeader = req.headers.token
-    if(authHeader){
-        const token=authHeader.split(" ")[1];
-        jwt.verify(token, process.env.JWT_SEC,(err,user) =>{
-            if(err){
-                res.status(403).json("Token not assigned");
-            }
-            req.user=user;
-            next();
-        });
-    }else{
-        return res.status(401).json("You are not authenticated");
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token
+  if (authHeader) {
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) {
+        res.status(403).json('Token not assigned')
+      }
+      req.user = user
+      next()
+    })
+  } else {
+    return res.status(401).json('You are not authenticated')
+  }
+}
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next()
+    } else {
+      res.status(403).json('You are not allowed to do that')
     }
+  })
 }
 
-const verifyTokenAndAuthorization = (req,res,next) =>{
-    verifyToken(req,res,()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin){
-            next();
-        }
-        else{
-            res.status(403).json("You are not allowed to do that");
-        }
-    })
+//for admin to add products
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next()
+    } else {
+      res.status(403).json('You are not allowed to do that')
+    }
+  })
 }
 
-//for admin to add products 
-const verifyTokenAndAdmin = (req,res,next) =>{
-    verifyToken(req,res,()=>{
-       
-        if(req.user.isAdmin){
-            next();
-        }
-        else{
-            res.status(403).json("You are not allowed to do that");
-        }
-    })
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
 }
-
-module.exports = {verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin};
